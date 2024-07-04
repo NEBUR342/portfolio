426dd3f7af9738d762de1aef3f5f569cdacfd24b
NEBUR342 <ralvarezfernandez342@gmail.com>
2024-07-04 12:29:13 +0200

    modifico colores

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
     frameworks: [],
     tecnologias: [],
     aptitudes: [],
-    detallesAdicionales: []
+    detallesAdicionales: [],
   });
 
   useEffect(() => {
@@ -29,13 +29,14 @@ const About = () => {
   const renderListSection = (
     title,
     items,
+    color,
     renderItem,
     animationClass = "fade-in"
   ) => (
     <div className="col-12 col-md-6 my-3">
       <CSSTransition timeout={500} classNames={animationClass}>
         <div className="card shadow-lg rounded-lg">
-          <div className="card-header bg-primary text-white text-center">
+          <div className={`card-header bg-${color} text-white text-center`}>
             <h3 className="mb-0">{title}</h3>
           </div>
           <div className="card-body p-4 rounded-5">
@@ -48,70 +49,62 @@ const About = () => {
     </div>
   );
 
-  const renderAptitudes = () =>
-    renderListSection("Aptitudes", data.aptitudes, (aptitud) => (
-      <li key={aptitud.id} className="list-group-item my-2">
-        <h4>{aptitud.nombre}</h4>
-      </li>
-    ));
-
-  const renderDetallesAdicionales = () =>
+  const renderExperiencias = () =>
     renderListSection(
-      "Detalles Adicionales",
-      data.detallesAdicionales,
-      (detalle) => (
-        <li key={detalle.id} className="list-group-item my-2">
-          <h4>{detalle.nombre}</h4>
+      "Experiencias",
+      data.experiencias,
+      "bloque1",
+      (experiencia) => (
+        <li key={experiencia.id} className="list-group-item my-2">
+          <h4>
+            <FontAwesomeIcon icon={faBuilding} className="me-2" />
+            {experiencia.nombreEmpresa}
+          </h4>
+          <h5>
+            <FontAwesomeIcon icon={faBriefcase} className="me-2" />
+            {experiencia.nombrePuesto}
+          </h5>
+          <p>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />
+            {experiencia.lugar}
+          </p>
+          <p>
+            {experiencia.fechaInicio} - {experiencia.fechaFin}
+          </p>
+          <p>{experiencia.descripcion}</p>
         </li>
       )
     );
 
-  const renderExperiencias = () =>
-    renderListSection("Experiencias", data.experiencias, (experiencia) => (
-      <li key={experiencia.id} className="list-group-item my-2">
-        <h4>
-          <FontAwesomeIcon icon={faBuilding} className="me-2" />
-          {experiencia.nombreEmpresa}
-        </h4>
-        <h5>
-          <FontAwesomeIcon icon={faBriefcase} className="me-2" />
-          {experiencia.nombrePuesto}
-        </h5>
-        <p>
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />
-          {experiencia.lugar}
-        </p>
-        <p>
-          {experiencia.fechaInicio} - {experiencia.fechaFin}
-        </p>
-        <p>{experiencia.descripcion}</p>
-      </li>
-    ));
-
   const renderVoluntariados = () =>
-    renderListSection("Voluntariados", data.voluntariados, (voluntariado) => (
-      <li key={voluntariado.id} className="list-group-item my-2">
-        <h4>
-          <FontAwesomeIcon icon={faBuilding} className="me-2" />
-          {voluntariado.nombreEmpresa}
-        </h4>
-        <h5>
-          <FontAwesomeIcon icon={faBriefcase} className="me-2" />
-          {voluntariado.nombrePuesto}
-        </h5>
-        <p>
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />
-          {voluntariado.lugar}
-        </p>
-        <p>
-          {voluntariado.fechaInicio} - {voluntariado.fechaFin}
-        </p>
-        <p>{voluntariado.descripcion}</p>
-      </li>
-    ));
+    renderListSection(
+      "Voluntariados",
+      data.voluntariados,
+      "bloque1",
+      (voluntariado) => (
+        <li key={voluntariado.id} className="list-group-item my-2">
+          <h4>
+            <FontAwesomeIcon icon={faBuilding} className="me-2" />
+            {voluntariado.nombreEmpresa}
+          </h4>
+          <h5>
+            <FontAwesomeIcon icon={faBriefcase} className="me-2" />
+            {voluntariado.nombrePuesto}
+          </h5>
+          <p>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="me-2" />
+            {voluntariado.lugar}
+          </p>
+          <p>
+            {voluntariado.fechaInicio} - {voluntariado.fechaFin}
+          </p>
+          <p>{voluntariado.descripcion}</p>
+        </li>
+      )
+    );
 
   const renderEstudios = () =>
-    renderListSection("Estudios", data.estudios, (estudio) => (
+    renderListSection("Estudios", data.estudios, "bloque2", (estudio) => (
       <li key={estudio.id} className="list-group-item my-2">
         <h4>
           <FontAwesomeIcon icon={faBuilding} className="me-2" />
@@ -132,7 +125,7 @@ const About = () => {
     ));
 
   const renderIdiomas = () =>
-    renderListSection("Idiomas", data.idiomas, (idioma) => (
+    renderListSection("Idiomas", data.idiomas, "bloque2", (idioma) => (
       <li key={idioma.id} className="list-group-item my-2">
         <h4>
           {idioma.nombre}: {idioma.nivel}
@@ -141,7 +134,7 @@ const About = () => {
     ));
 
   const renderLenguajes = () =>
-    renderListSection("Lenguajes", data.lenguajes, (lenguaje) => (
+    renderListSection("Lenguajes", data.lenguajes, "bloque3", (lenguaje) => (
       <li key={lenguaje.id} className="list-group-item my-2">
         <h4>{lenguaje.nombre}</h4>
         <p>{lenguaje.descripcion}</p>
@@ -149,7 +142,7 @@ const About = () => {
     ));
 
   const renderFrameworks = () =>
-    renderListSection("Frameworks", data.frameworks, (framework) => (
+    renderListSection("Frameworks", data.frameworks, "bloque3", (framework) => (
       <li key={framework.id} className="list-group-item my-2">
         <h4>{framework.nombre}</h4>
         <p>{framework.descripcion}</p>
@@ -157,12 +150,36 @@ const About = () => {
     ));
 
   const renderTecnologias = () =>
-    renderListSection("Tecnologías", data.tecnologias, (tecnologia) => (
-      <li key={tecnologia.id} className="list-group-item my-2">
-        <h4>{tecnologia.nombre}</h4>
-        <p>{tecnologia.descripcion}</p>
+    renderListSection(
+      "Tecnologías",
+      data.tecnologias,
+      "bloque3",
+      (tecnologia) => (
+        <li key={tecnologia.id} className="list-group-item my-2">
+          <h4>{tecnologia.nombre}</h4>
+          <p>{tecnologia.descripcion}</p>
+        </li>
+      )
+    );
+
+  const renderAptitudes = () =>
+    renderListSection("Aptitudes", data.aptitudes, "bloque4", (aptitud) => (
+      <li key={aptitud.id} className="list-group-item my-2">
+        <h4>{aptitud.nombre}</h4>
       </li>
     ));
+
+  const renderDetallesAdicionales = () =>
+    renderListSection(
+      "Detalles Adicionales",
+      data.detallesAdicionales,
+      "bloque4",
+      (detalle) => (
+        <li key={detalle.id} className="list-group-item my-2">
+          <h4>{detalle.nombre}</h4>
+        </li>
+      )
+    );
 
   return (
     <div className="container-fluid">
