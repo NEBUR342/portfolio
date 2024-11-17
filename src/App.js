5f03bcf723b7e50c166e289e99b7aaaedf076a00
NEBUR342 <ralvarezfernandez342@gmail.com>
2024-11-17 17:00:10 +0100

    cambio rutas

--- src/App.js
+++ src/App.js
@@ -22,14 +22,14 @@ const Header = () => (
       <div className="row align-items-center justify-content-between mx-1 h-100">
         <div className="col">
           <h2 className="my-auto">
-            <Link to="/portfolio" className="link-unstyled" aria-label="Home">
+            <Link to="" className="link-unstyled" aria-label="Home">
               <FontAwesomeIcon icon={faHouse} className="icon-home" />
             </Link>
           </h2>
         </div>
         <div className="col d-flex justify-content-end">
-          <Link to="/portfolio/contact" className="mx-1" aria-label="Contact">
-            <button className="btn btn-info">Contacto</button>
+          <Link to="/contact" className="mx-1">
+            <button className="btn btn-info">Contact</button>
           </Link>
         </div>
       </div>
@@ -40,8 +40,7 @@ const Header = () => (
 const Main = () => (
   <main className="container-fluid main-content fondogradiente py-1 flex-grow-1">
     <Routes>
-      <Route path="/portfolio" element={<Home />} />
-      <Route path="/portfolio/contact" element={<Contact />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="" element={<Home />} />
     </Routes>
   </main>
